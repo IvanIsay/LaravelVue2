167c0e62e79cc701cc737ba62fe08e18ccc2cdd7
lOkY <102552151+IvanIsay@users.noreply.github.com>
2022-12-15 15:28:34 -0600

    Preparacion funciones controlador

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,82 +4,76 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+
+    public function index(){
+       //consulta los libros en el modelo y se guarda
+        $books = Book::all();
+    
+        //Abre la vista Index y le pasamos la consulta
+       return Inertia::render('Books/Index',['books' => $books]);
+
+    } 
+
+
+    public function create(){
+
+        return Inertia::render('Books/Create');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+
+    public function store(Request $request){
+        $request->validate([
+            'title' => 'required',
+            'autor' => 'required',
+            'review' => 'required'
+        ]);
+
+        Book::create([
+            'title' => $request->title,
+            'autor' => $request->autor,
+            'review' => $request->review
+        ]);
+        sleep(1);
+
+        return redirect()->route('books.index')->with('message', 'Book Created Succesfully');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function edit(Book $book){
+
+    return Inertia::render( 'Books/Edit',[ 'book' => $book]);
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Book $book)
-    {
-        //
+    public function update(Request $request, Book $book){
+
+        $request->validate([
+            'title' => 'required',
+            'autor' => 'required',
+            'review' => 'required'
+        ]);
+
+        $book->title = $request->title;
+        $book->autor = $request->autor;
+        $book->review = $request->review;
+        $book->save();
+        sleep(1);
+
+        return redirect()->route('books.index')->with('message', 'Book Updated Successfully');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Book $book)
-    {
-        //
+    public function destroy(Book $book){
+
+        $book->delete();
+        sleep(1);
+        
+        return redirect()->route('books.index')->with('message', 'Book Delete Successfully');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Book $book)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Book $book)
-    {
-        //
-    }
+
+    
 }
